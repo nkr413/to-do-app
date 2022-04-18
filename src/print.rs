fce867e0f4573254df948948eb60ede030872488
nkr <nkgithubnarkuziev108@gmail.com>
2022-04-18 14:57:29 +0500

    mini update 0.3

--- src/print.rs
+++ src/print.rs
@@ -2,14 +2,54 @@ pub mod print_func {
 	extern crate rusqlite;
 	use rusqlite::{params, Connection, Result, NO_PARAMS};
 
+	#[derive(Debug)]
+	struct Type {
+		id: i64,
+		text: String
+	}
+
+	#[derive(Debug)]
+	struct Note {
+		id: i64,
+		text: String,
+		status: String,
+		list: String
+	}
+
 	pub fn print_all() -> Result<()> {
 		println!("all-data");
+		let conn = Connection::open("base.db3")?;
+		let mut list = conn.prepare("SELECT id, text FROM lists")?;
+		let data = list.query_map([], |row| {
+			Ok(Type {
+				id: row.get(0)?,
+				text: row.get(1)?,
+			})
+		})?;
 
+		let mut v = Vec::new();
+		for i in data {v.push(i.unwrap());}
 
+		println!("{:?}", v);
 		Ok(())
 	}
 
 	pub fn print_one() -> Result<()> {
+		pub fn get_list() -> Result<()> {
+			let conn = Connection::open("base.db3")?;
+			let mut list = conn.prepare("SELECT id, text, status, list FROM base")?;
+			let data = list.query_map([], |row| {
+				Ok(Note {
+					id: row.get(0)?,
+					text: row.get(1)?,
+					status: row.get(2)?,
+					list: row.get(3)?,
+				})
+			})?;
+
+			Ok(())
+		}
+
 		println!("Which list do you want to see ? -->");
 
 		let mut resp = String::new();
